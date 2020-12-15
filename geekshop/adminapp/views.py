30ea8c54eb759b6d25cc58bdfbc689006a216821
di-mario666 <di-mario@mail.ru>
2020-12-15 20:00:19 +0300

    дз

---
+++ geekshop/adminapp/views.py
@@ -0,0 +1,145 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.urls import reverse
+from adminapp.forms import ShopUserAdminEditForm
+from authapp.forms import ShopUserRegisterForm
+from authapp.models import ShopUser
+from mainapp.models import ProductCategory, Product
+
+
+# пользователи
+@user_passes_test(lambda u: u.is_superuser)
+def user_create(request):
+    title = 'пользователи/создание'
+
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {'title': title, 'update_form': user_form}
+
+    return render(request, 'adminapp/user_update.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def users(request):
+    title = 'админка/пользователи'
+
+    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+
+    content = {
+        'title': title,
+        'objects': users_list
+    }
+
+    return render(request, 'adminapp/users.html', content)
+
+
+def user_update(request, pk):
+    title = 'пользователи/редактирование'
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {
+        'title': title,
+        'update_form': edit_form
+    }
+
+    return render(request, 'adminapp/user_update.html', content)
+
+
+def user_delete(request, pk):
+    title = 'пользователи/удаление'
+
+    user_item = get_object_or_404(ShopUser, pk=pk)
+    # для полного удаления пользователя
+    # user_item.delete()
+
+    # для скрытия пользователя
+    if request.method == 'POST':
+        if user_item.is_active:
+            user_item.is_active = False
+        else:
+            user_item.is_active = True
+        user_item.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    content = {
+        'title': title,
+        'user_to_delete': user_item
+    }
+
+    return render(request, 'adminapp/user_delete.html', content)
+
+
+# категории
+def category_create(request):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+    title = 'админка/категории'
+
+    categories_list = ProductCategory.objects.all().order_by('-is_active')
+
+    content = {
+        'title': title,
+        'objects': categories_list
+    }
+
+    return render(request, 'adminapp/categories.html', content)
+
+
+def category_update(request, pk):
+    pass
+
+
+def category_delete(request, pk):
+    pass
+
+
+# продукты
+def product_create(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def products(request, pk):
+    title = 'админка/продукт'
+
+    category_item = get_object_or_404(ProductCategory, pk=pk)
+    products_list = Product.objects.filter(category=category_item)
+
+    content = {
+        'title': title,
+        'category': category_item,
+        'objects': products_list
+    }
+
+    return render(request, 'adminapp/products.html', content)
+
+
+def product_read(request, pk):
+    pass
+
+
+def product_update(request, pk):
+    pass
+
+
+def product_delete(request, pk):
+    pass
+
+
